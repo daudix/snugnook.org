53e516195b5cb4572fb29d54bf7323b6ca88d069
daudix <ddaudix@gmail.com>
2026-01-25 03:49:45 +0300

    Update emoji map

--- make-emoji-map.js
+++ make-emoji-map.js
@@ -14,7 +14,7 @@ https.get("https://emzi0767.mzgit.io/discord-emoji/discordEmojiMap.json", (res) 
 			emojiMap[def.primaryName] = def.surrogates;
 		}
 
-		fs.writeFileSync("emoji-map.json", JSON.stringify(emojiMap, null, 2));
+		fs.writeFileSync("content/emoji-map.json", JSON.stringify(emojiMap, null, 2));
 		console.log("Saved emoji-map.json with", Object.keys(emojiMap).length, "entries");
 	});
 });
