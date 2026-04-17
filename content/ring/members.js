c615e2099d81553f7b262edccc00c8c613d7c05e
daudix <ddaudix@gmail.com>
2026-04-17 14:54:44 +0300

    Add vuvien to webring

--- content/ring/members.js
+++ content/ring/members.js
@@ -1,22 +1,26 @@
 const members = [
-	{
-		url: "https://daudix.one/",
-		title: "Daudix",
-	},
-	{
-		url: "https://immjs.dev/",
-		title: "Informa",
-	},
-	{
-		url: "https://kevadesu.github.io/",
-		title: "kevadesu",
-	},
-	{
-		url: "https://reduxflakes.nekoweb.org/",
-		title: "ReduxFlakes",
-	},
-	{
-		url: "https://anins1der.com/",
-		title: "anins1der",
-	},
+  {
+    url: "https://daudix.one/",
+    title: "Daudix",
+  },
+  {
+    url: "https://immjs.dev/",
+    title: "Informa",
+  },
+  {
+    url: "https://kevadesu.github.io/",
+    title: "kevadesu",
+  },
+  {
+    url: "https://reduxflakes.nekoweb.org/",
+    title: "ReduxFlakes",
+  },
+  {
+    url: "https://anins1der.com/",
+    title: "anins1der",
+  },
+  {
+    url: "https://vuvien.xyz/",
+    title: "vuvien",
+  },
 ];
